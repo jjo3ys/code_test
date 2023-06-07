584f6f6c9d695731a10de53743df04d83c61079a
yeonsung Joe <jjo3ys@gmail.com>
2023-06-07 18:13:07 +0900

    solve problem with Top class

--- baekjoon/stack/s2493.java
+++ baekjoon/stack/s2493.java
@@ -6,26 +6,33 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+class Top{
+    int index;
+    int height;
+    Top(int index, int height){
+        this.index = index;
+        this.height = height;
+    }
+}
+
 public class s2493 {
+    
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        Stack<Integer> stack = new Stack<>();
+        Stack<Top> stack = new Stack<>();
         int target;
-        int count;
         
         for(int i=0;i<n;i++){
             target = Integer.parseInt(st.nextToken());
-            count = 0;
-            while(!stack.isEmpty()&&stack.peek()<=target){
+            while(!stack.isEmpty()&&stack.peek().height<target){
                 stack.pop();
-                count++;
             }
             if (stack.isEmpty()) System.out.print("0 ");
-            else System.out.print(i-count+" ");
-            stack.push(target);       
+            else System.out.print(stack.peek().index+" ");
+            stack.push(new Top(i+1, target));       
         }
     }
 }
